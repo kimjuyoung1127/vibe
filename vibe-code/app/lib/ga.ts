4ba5c2c0d7069c18a2b9ac26b4b7f22c9e425db6
김주영 <gmdqn2tp@email.com>
2025-09-30 23:22:15 +0900

    dd

--- vibe-code/app/lib/ga.ts
+++
@@ -1,6 +0,0 @@
-// app/lib/ga.ts
-import { GoogleAnalytics } from '@next/third-parties/google';
-
-export default function GoogleAnalyticsWrapper({ GA_MEASUREMENT_ID }: { GA_MEASUREMENT_ID: string }) {
-  return <GoogleAnalytics gaId={GA_MEASUREMENT_ID} />;
-}
